4b173761d5f53d61ca621e1cfdc40646a4fb29cd
Aman Jain <91aman@gmail.com>
2013-04-05 15:57:27 +0530

    main page

---
+++ index.php
@@ -0,0 +1,726 @@
+
+<html>
+<head>
+<title>DA-IICT Sports</title>
+</head>
+
+<body style="background:#fff;">
+
+	<style>
+		* {z-index:0;}
+		div#container{
+	 		background: #ccc; 
+			background-image: url(http://upload.wikimedia.org/wikipedia/en/5/55/DAIICT_OAT.JPG);
+			background-repeat:no-repeat;
+			background-attachment: fixed;
+			background-position:center;
+	 		position:relative;
+			margin:-10px -8px;
+	 		border-style:solid;
+			padding-bottom:5px;
+			overflow-y:hidden;
+	 		overflow-x:hidden;
+			}
+	 </style>
+     <div id="container">
+
+        <div style="width:960px;margin:auto;"> 
+        	<img width="960px" height="3000px" style="z-index:-1;opacity:1;position:absolute;" src="C:\Users\SAHIL\Desktop\Sahil.jpg"  />
+        </div>
+        
+        
+     		    
+
+<style>
+	div#head-top{
+		width:100%;
+		background: #0066CC;
+				position:fixed;
+		height:30px;
+		padding: 0px 0px;
+		border:1px solid black;
+		border-left-style:none;
+		border-right-style:none;
+		margin-top:-3px;
+		z-index:1000;
+		}
+
+</style>
+
+<div id="head-top">
+	<div style="background:#0066CC;float:left;margin-top:0px;position:relative;margin-left:75px;padding-right:5px;border:0px solid #000;">
+    <style>
+		div#daiict-logo{
+			float:left;
+			padding:0px;
+			margin-left:7px;
+			background:#0066CC;
+			margin-top:3px;		
+			}
+
+	</style>
+    <div id="daiict-logo">
+    	<img height="25px" width="25px;" style=" border:0px solid #fff;" src="http://upload.wikimedia.org/wikipedia/commons/9/92/Dhirubhai_Ambani_Institute_of_Information_and_Communication_Technology.gif" />
+    </div>
+	<style>
+		div#daiict-name{
+			float:left;
+			padding:8px 0px;
+			margin-top:1px;
+			margin-left:6px;
+			font: 12px Verdana, Geneva, sans-serif;
+			font-size:11px;
+			text-transform: uppercase;
+			color:#fff;
+			}
+	</style> 
+	<div id = "daiict-name">DHIRUBHAI AMBANI-INSTITUTE OF INFORMATION AND COMMUNICATION TECHNOLOGY</div>
+    </div>    
+	<style>
+		div#other-links{
+			float:right;
+			margin-right:75px;
+			margin-top:0px;
+			background:#0066CC;
+			border:0px solid #000;
+			font: 12px Verdana, Geneva, sans-serif;
+			font-size:11px;
+			padding:8px 5px;
+
+			}
+		div#other-links a{
+			text-decoration:none;
+			color:#fff;
+			}
+		div#other-links a:hover{
+			text-decoration: underline;
+			color:#fff;
+			}
+
+	</style>
+    <div id="other-links">
+    	<a href="http://www.daiict.ac.in/daiict/index.html">DA-IICT Home</a> <span style="color:#666;font-weight:bold;">|</span> 
+        <span style="color:#666;font-weight:bold;">|</span> <a href="">Contact Us</a> 
+    </div>
+    <div style="clear:both;">
+    </div>
+
+</div>
+
+<style>
+	#head-top-compensate{
+		width:100%;
+		background: #4f0015;
+		height:30px;
+		padding: 0px 0px;
+		border:1px solid black;
+		border-left-style:none;
+		border-right-style:none;
+		margin-top:-3px;
+		z-index:1000;		
+		}
+
+</style>    
+<div id="head-top-compensate"></div>        
+        
+     		
+	   	<style>
+
+#topannounce a{
+	text-decoration:none;
+	color:#352a2a;
+	}
+#topannounce a:hover{
+	text-decoration:underline;
+	color:#7d4900;
+	}
+</style>
+
+
+<style>
+	div#header{
+		margin: auto;
+		width: 918px;
+		height: 200px;
+		overflow-y:hidden;
+		background:#666666;
+		border:1px solid #ccc;
+		margin-top:6px;
+
+		background-image:url();
+
+		}
+
+</style>
+<div id="header" >
+<img src="http://sbg.daiict.ac.in/sbg/images/concours.jpg" title="daiict Home" width = 918px;
+		height= 200px;><div style="width:918px; height:200px;"></div>
+</a>
+</div>
+        
+           <style>
+            div.gap{
+				height:6px;
+				width:910px;
+				margin:auto;
+					}
+			</style>
+            <div class="gap"></div>
+            
+<style>
+	div#menubar{
+		margin:auto;
+		margin-top:-2px;
+	   	width: 918px;
+		height: 16px;
+		padding:5px 0px;
+		padding-top:8px;
+		background:url();
+		background-color:#0066CC;
+		background-repeat:repeat-x;
+		border:1px solid #666; 
+
+		overflow:auto;
+
+
+		}
+</style>
+<div id="menubar">
+
+    <style>
+		#menuitem {
+		    float: left;
+			padding:0px 14px;
+			border-right:1px solid #fff;
+  			}
+
+
+		#menuitema {
+		    border-radius:;
+		    border:0px solid #fff;
+		    font:12px Tahoma, Geneva, sans-serif;
+		    font-size: 12px;
+	        font-weight:bold;
+		    color: #fff;
+		    text-decoration: none; 
+			padding:2px 6px;
+
+   			}
+
+		#menuitem ul {
+		    display: none; 
+		    width:; 
+		    background-color: #fff;
+		   }
+
+		#menuitem:hover #menuitema{
+			background:#fff;
+			color:#7d4900;
+			border-top-right-radius:2px;
+			border-top-left-radius:2px;
+			}
+
+		#menuitem:hover ul{
+		    display: block;
+		    position: absolute;
+		    margin: 0;
+			margin-left:0px;
+			margin-top:2px;
+		    padding-left:0px; ;
+		    border-bottom:1px solid #666;
+			z-index:1000;
+			list-style:none;			
+    		}
+
+		#menuitem:hover li{
+			float: none;
+		    width:9em;
+			margin:0px;
+		    margin-left:0px;
+			border:1px solid #666;
+			border-bottom:none;			
+    		}
+
+		#menuitem:hover li a{
+		 	display:block;
+			padding:3px;
+			padding-left:5px;
+   			color: #352a2a;
+		    border-radius:.25em;
+		    font:12px Tahoma Tahoma, Geneva, sans-serif;
+		    font-size: 12px;
+		    font-weight:;
+			text-decoration:none; 
+			box-shadow:none;
+			}
+   
+		#menuitem li a:hover {
+		    background-color: #ccc; 
+		    color:#000;
+			}
+
+	</style>
+
+    	<div id="menuitem" style="margin-left:2px;"><a id="menuitema" style="border-bottom-right-radius:2px; border-bottom-left-radius:2px;" href="/Daiictsportswebsite/index.php">HOME</a>
+        </div>
+        
+        <div id="menuitem"><a id="menuitema" href="#">SPORTS</a>
+          <ul>
+           	  <li><a href="sports/badminton.php">Badminton</a></li>
+              <li><a href="sports/basketball.php">Basketball</a></li>
+              <li><a href="sports/cricket.php">Cricket</a></li>
+              <li><a href="sports/football.php">Football</a></li>
+              <li><a href="sports/tabletennis.php">Table Tennis</a></li>
+              <li><a href="sports/volleyball.php">Volleyball</a></li></ul>
+        </div>
+        
+        <div id="menuitem"><a id="menuitema" href="#">EVENTS</a>
+        	<ul>
+               	<li><a href="events/concours.php">CONCOURS</a></li>
+           	  <li>
+              	 <a href="events/interbatch.php">INTER-BATCH</a></li>
+           	  <li>
+              	 <a href="events/interwing.php">INTER-WING</a></li>
+           	  <li>
+              	
+        </div>
+        
+        <div id="menuitem"><a id="menuitema" href="#">FACILITIES</a>
+          <ul>
+          <li><a href="facilities/badminton.php">Badminton</a></li>
+              <li><a href="facilities/basketball.php">Basketball</a></li>
+              <li><a href="facilities/cricket.php">Cricket</a></li>
+              <li><a href="facilities/football.php">Football</a></li>
+              <li><a href="facilities/tabletennis.php">Table Tennis</a></li>
+              <li><a href="facilities/volleyball.php">Volleyball</a></li></ul>         	
+        </div>
+        
+        <div id="menuitem"><a id="menuitema" href="renteditems/renteditems.php">RENT</a></div>
+      		
+        <div id="menuitem"><a id="menuitema" href="studentactivities/studentactivities.php" style="border-bottom-right-radius:2px; border-bottom-left-radius:2px;">STUDENT'S ACTIVITY</a>
+
+		</div>
+
+        <div id="menuitem"><a id="menuitema" href="#">PEOPLE</a>
+			<ul>
+               	<li><a href="people/administration.php">Administration</a></li>
+                <li><a href="people/sportscommittee.php">Sports Committee</a></li></ul>
+		</div>
+      		
+            <div id="menuitem"><a id="menuitema" href="#">MISC.</a>
+			<ul>
+               	<li><a href="misc/awards.php">Institute Awards</a></li>
+              </ul>         
+		</div>
+        		
+        <div id="menuitem" style="padding-right:8px;border:none;"><a id="menuitema" style="border-bottom-right-radius:2px; border-bottom-left-radius:2px;" href="registration/registration.php">REGISTRATIONS</a>         
+		</div>
+      		
+	   </div>
+        
+        <div class="gap"></div>
+
+<style>
+	div#content-box{
+		margin:auto;
+		width: 910px;
+		background:#ccc;
+		border:1px solid #ccc;
+		padding: 10px 4px;
+		}
+</style>
+<div id="content-box">
+
+     <div style="float:left">           	
+    <style>
+		div#textinfo-box{
+			width:570px;
+			height: ;
+			border:1px solid #ccc;
+			margin-left:6px;
+			margin-right:8px;
+			background:#fff;
+			}
+
+	</style>
+    <div id="textinfo-box">
+    	               
+		<style>
+			#tbmediabox{
+				width:500px;
+				margin:auto;
+				border:1px solid #ccc;
+
+				}
+
+		</style>
+        <script language="JavaScript" type="text/javascript">  
+var images = new Array('01.jpg','02.jpg','03.jpg','04.jpg')  
+var count = -1  
+  
+function slideShow(){    
+  
+ if (count <=2){    
+  count ++;  
+ }  
+  else  
+ count = 0; 
+ 
+  
+  document.getElementById("show").innerHTML = '<img src="images/'+images[count]+'" height="350px" width="570px;" alt="123" />'   
+  setTimeout("slideShow()", 3000)    
+}    
+</script>  
+<div id="show">  
+ <script>slideShow()</script>  
+</div> 
+
+    	         
+         <style>
+		 	#tbtext{
+				margin-left:20px;
+				margin-right:10px;
+				text-align:left;
+				color:#352a2a;
+				font:12px Tahoma, Geneva, sans-serif;
+				font-size:12px;
+				line-height:20px;
+				text-indent:0px;
+				text-align:justify;}
+		 </style>
+    	<div id="tbtext">
+    	<p>
+ <p><center><b><i>
+"If you are locked in a room with books for two years and throw away the key, you can probably make it into DA-IICT".</center></b></i></p>
+
+<p>Turns out, you can pretty much do the same once you're in DA-IICT too.  Except now, you can leave the books inside, and lock yourself out of the room. Because such is the variety and vibrancy of extracurricular life in DA-IICT, you're destined to find worthwile and meaningful activities to pursue for every  year you spend in this campus. And DA-IICT Sports is one of the more brilliant facets of this life.</p>
+
+<p>We possess a good sports infrastructure which includes Sports Activity Center(SAC), Football and Cricket grounds, volleball and basketball courts. </p>
+
+<p>But more importantly, we cater to all degrees of enthusiasm too. From the most intensive training camps for Inter-college sports, to daily recreational football to playing old-school Sitoliya  during specially organised Sports Fests. We, quite honestly, have something for everyone. Or you could live your IPL/EPL dreams by playing in our semester long leagues. The possibilities are endless.</p>
+
+<p>So come, re-live the years spent on school playgrounds and experience the joy of newfound and like-minded friends, the fervour of cheering for your wings, the ultimate glory of a DA-IICT medal.</p>
+
+<p>It all begins here, at DA-IICT Sports.</p></p>
+		</div>
+        
+    </div>           
+    
+    <style>
+		div#newsinfo-box{
+			width:570px;
+			height: 178px;
+			margin-top:8px;
+			border:1px solid #ccc;
+			margin-left:6px;
+			margin-right:8px;
+			background:#fff;
+			}
+				.nbhead{
+						margin:auto;
+						margin-top:0px;
+						padding:6px 2px;
+						background:#0066CC;
+						border-bottom:1px solid black;
+						color:#fff;
+						font:12px  Tahoma, Geneva, sans-serif;
+						font-size:12px;
+						text-align:center;
+						font-weight:bold;
+						}
+	</style>
+    
+    </div>             
+    <style>
+		div#picinfo-box{
+			width:322px;			
+			float:left;
+			}
+	</style>
+    <div id="picinfo-box">
+    	 	       	
+        <style>
+			div#pb1{
+				width:316px;
+				border:1px solid #ccc;
+				float:left;
+				padding-bottom:5px;
+				background:#fff;
+				}
+		</style>
+        <div id="pb1">
+            	
+                <style>
+					.pbhead{
+						margin:auto;
+						margin-top:0px;
+						padding:6px 2px;
+						background:#0066CC;
+						border-bottom:1px solid black;
+						color:#fff;
+						font:12px  Tahoma, Geneva, sans-serif;
+						font-size:12px;
+						text-align:center;
+						font-weight:bold;
+						}
+
+				</style>
+                <div class="pbhead"> SPORTS AWARDS '12
+                </div>	
+                
+                
+               <style>
+					#pb1iteml{
+
+						background:#f1f1f1;
+						width:130px;
+						margin-left:19px;
+						margin-right:9px;
+						margin-top:8px;
+						padding:5px 0px;
+						text-align:center;
+						float:left;
+
+						}
+					#pb1itemr{
+
+						background:#f1f1f1;
+						width:130px;
+						margin-left:9px;
+						margin-right:19px;
+						margin-top:8px;
+						padding:5px 0px;
+						text-align:center;
+						float:left;
+
+						}
+					#pb1image{
+						margin-top:10px;
+						margin-bottom:5px;
+						}
+					.pb1text{
+						font:12px Tahoma, Geneva, sans-serif;
+						font-size:12px;
+						color:#352a2a;
+						line-height:16px;
+						}
+					#pb1itemname{
+						font-size:13px;
+						}
+					#pb1itemspec{
+						font-size:14px;
+						color:#7d4900;
+						margin-top:0px;
+
+						}	
+				</style>
+                <div id="pb1iteml">
+                	
+                    <div class="pb1text" id="pb1itemspec">Person of the Year</div>
+                	<div id="pb1image">
+                		<img src="" height="100px" width="100px" style="border:1px solid #ccc;" />
+                	</div>
+                	
+                	<div class="pb1text" id="pb1itemname"><br /><span style="font-size:12px;"></span></div>
+                		
+
+                </div>
+                
+                <div id="pb1itemr">
+                	
+                    <div class="pb1text" id="pb1itemspec"></div>
+                	<div id="pb1image">
+                		<img src="" height="100px" width="100px" style="border:1px solid #ccc;" />
+                	</div>
+                	
+                	<div class="pb1text" id="pb1itemname"><br /><span style="font-size:12px;"></span></div>
+                		
+
+                </div>
+                <div style="clear:both;"></div>
+                
+                <style>
+				#redirect{
+					text-align:left;
+					font:12px Tahoma, Geneva, sans-serif;
+					font-size:11px;
+					margin-top:12px;
+					margin-left:16px;
+					margin-bottom:4px;}
+				#redirect a{
+					text-decoration: none;
+					color: #352a2a;
+					border:1px solid #fff;
+					background:#fff;
+					border-radius:2px;
+					padding:4px;}
+				#redirect a:hover{
+					border:1px solid #fff;
+					color:#000;
+					text-decoration:underline;
+										}
+				</style>
+              <div id="redirect" ><a  href="">View all awards...</a></div>
+             
+        </div>
+        
+ 		<style>
+			div#pb2{
+				width:316px;
+				border:1px solid #ccc;
+				float:left;
+				margin-top:8px;
+				padding-bottom:5px;
+
+				background:#fff;
+				text-align:center;
+				display:;
+				}	
+		</style>       
+        <div id="pb2">
+            	
+                <div class="pbhead">
+                </div>
+                <style>
+					#pb2iteml{
+
+						background:#f1f1f1;
+						width:130px;
+						padding:5px 0px;
+						float:left;
+						margin-left:19px;
+						margin-right:9px;
+						margin-top:8px;
+
+						}
+					#pb2itemr{
+
+						background:#f1f1f1;
+						width:130px;
+						padding:5px 0px;
+						float:left;
+						margin-left:9px;
+						margin-right:19px;
+						margin-top:8px;
+
+						}
+
+					#pb2image{
+						margin-top:10px;
+						margin-bottom:5px;
+						}
+					.pb2text{
+						font:12px Tahoma, Geneva, sans-serif;
+						font-size:12px;
+						color:#352a2a;
+						line-height:16px;
+						}
+					#pb2itemname{
+						font-size:13px;
+						}
+					#pb2itemspec{
+						font-size:14px;
+						color:#7d4900;
+						}	
+				</style>
+                <div id="pb2iteml">
+                	<div class="pb2text" id="pb2itemspec"></div>
+                    <div id="pb2image">
+                		<img src="" height="100px" width="100px" style="border:1px solid #ccc;" />
+                	</div>
+                	
+                	<div class="pb2text" id="pb2itemname"><br /><span style="font-size:12px;"></span></div>
+                		
+
+                </div>
+                
+                 <div id="pb2itemr">
+                	<div class="pb2text" id="pb2itemspec"></div>
+                    <div id="pb2image">
+                		<img src="" height="100px" width="100px" style="border:1px solid #ccc;" />
+                	</div>
+                	
+                	<div class="pb2text" id="pb2itemname"><br /><span style="font-size:12px;"></span></div>
+                						
+                </div>
+                
+                <div style="clear:both"></div>
+                
+                
+                
+              <div id="redirect" ><a  href=""></a></div>
+      </div>
+            
+            <style>
+			div#pb3{
+				width:316px;
+				/*height: 240px;*/
+				border:1px solid #ccc;
+				float:left;
+				margin-top:8px;
+				padding-bottom:0px;
+				background:#fff;
+				text-align:center;
+
+				}	
+		</style> 
+       
+            
+    </div>
+    
+    
+    
+    
+    <div style="clear:both;"></div>
+    
+       
+       
+</div>
+
+<style>
+	div#quickinfo-box{
+		margin:auto;
+		margin-top:0px;
+		margin-bottom:4px;
+		width: 910px;
+		padding-left:-40px;
+		}
+		
+</style>
+  
+<div id="quickinfo-box">
+  <iframe src="/Daiictsportswebsite/quicklink.php" width="920px" height="330px" marginheight="0px" marginwidth="0" seamless scrolling="no"> </iframe>
+</div>
+
+<style>
+	div#footer{
+	width:100%;
+	background-color: #0066CC;
+	border:1px solid #000;
+	border-left-style:none;
+	border-right-style:none;
+	text-align:center;
+	padding:0px 0px;
+	paddin-bottom:0px;
+	font: 12px  Verdana, Geneva, sans-serif ;
+	font-size:12px;
+	font-weight:;
+	color:#fff;
+	margin-bottom:-8px;
+	line-height:10px;
+	}
+</style>
+<div id="footer">
+	<div style="font-size:11px;line-height:15px;margin-top:4px;">
+    	SPORTS COMMITTEE, DA-IICT, GANDHINAGAR </div> <div style="font-size:10px;line-height:15px;margin-bottom:4px;">Developed by SAHIL SIKKA, AMAN JAIN & KRISH MAHAJAN 
+
+	</div>
+
+
+</div>
+
+ </div> 
+
+
+</body> 
+</html> 
